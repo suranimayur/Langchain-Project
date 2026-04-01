3425891fe70d601d94d1623675dbdb9ea605bb22
suranimayur <surani.mayur@gmail.com>
2026-04-01 22:05:50 +0530

    Intial project comment

--- main.py
+++ main.py
@@ -4,93 +4,29 @@ import os
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 
+@tool
+def search_query(query:str):
+    """Tool thast search for a Internet
+    
+    Args: 
+        The query to search for
 
-def main():
-    print("Hello from build-ai-agents-with-langchain!")
-
-    information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2025; as of February 2026, Forbes estimates his net worth to be around US$852 billion.
-
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
-
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals.
-
-Musk was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated as president in early 2025, Musk served as Senior Advisor to the President and as the de facto head of the Department of Government Efficiency (DOGE). After a public feud with Trump, Musk left the Trump administration and returned to managing his companies. Musk is a supporter of global far-right figures, causes, and political parties. His political activities, views, and statements have made him a polarizing figure. Musk has been criticized for COVID-19 misinformation, promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service, following his pledge to decrease censorship. His role in the second Trump administration attracted public backlash, particularly in response to DOGE. The emails he sent to Jeffrey Epstein are included in the Epstein files, which were published between 2025–26 and became a topic of worldwide debate.
-
-Early life
-See also: Musk family
-Elon Reeve Musk was born on June 28, 1971, in Pretoria, South Africa's administrative capital.[1][2] He is of British and Pennsylvania Dutch ancestry.[3][4] His mother, Maye (née Haldeman), is a model and dietitian born in Saskatchewan, Canada, and raised in South Africa.[5][6][7] Musk therefore holds both South African and Canadian citizenship from birth.[8] His father, Errol Musk, is a South African electromechanical engineer, pilot, sailor, consultant, emerald dealer, and property developer, who partly owned a rental lodge at Timbavati Private Nature Reserve.[9][10][11][12]
-
-His maternal grandfather, Joshua N. Haldeman, who died in a plane crash when Elon was a toddler, was an American-born Canadian chiropractor, aviator and political activist in the technocracy movement[13][14] who moved to South Africa in 1950.[15]
-
-Elon has a younger brother, Kimbal, a younger sister, Tosca, and four paternal half-siblings.[16][17][7][18] Musk was baptized as a child in the Anglican Church of Southern Africa.[19][20] Despite both Elon and Errol previously stating that Errol was a part owner of a Zambian emerald mine,[12] in 2023, Errol recounted that the deal he made was to receive "a portion of the emeralds produced at three small mines".[21][22] Errol was elected to the Pretoria City Council as a representative of the anti-apartheid Progressive Party and has said that his children shared their father's dislike of apartheid.[1]
-
-After his parents divorced in 1979, Elon, aged around 9, chose to live with his father because Errol Musk had an Encyclopædia Britannica and a computer.[23][3][9] Elon later regretted his decision and became estranged from his father.[24] Elon has recounted trips to a wilderness school that he described as a "paramilitary Lord of the Flies" where "bullying was a virtue" and children were encouraged to fight over rations.[25] In one incident, after an altercation with a fellow pupil, Elon was thrown down concrete steps and beaten severely, leading to him being hospitalized for his injuries.[26] Elon described his father berating him after he was discharged from the hospital.[26] Errol denied berating Elon and claimed, "The [other] boy had just lost his father to suicide, and Elon had called him stupid. Elon had a tendency to call people stupid. How could I possibly blame that child?"[27]
-
-Elon was an enthusiastic reader of books, and had attributed his success in part to having read The Lord of the Rings, the Foundation series, and The Hitchhiker's Guide to the Galaxy.[11][28] At age ten, he developed an interest in computing and video games, teaching himself how to program from the VIC-20 user manual.[29] At age twelve, Elon sold his BASIC-based game Blastar to PC and Office Technology magazine for approximately $500 (equivalent to $1,600 in 2025).[30][31]
-
-Education
-An ornate school building
-Musk graduated from Pretoria Boys High School in South Africa.
-Musk attended Waterkloof House Preparatory School, Bryanston High School, and then Pretoria Boys High School, where he graduated.[32] Musk was a decent but unexceptional student, earning a 61/100 in Afrikaans and a B on his senior math certification.[33] Musk applied for a Canadian passport through his Canadian-born mother to avoid South Africa's mandatory military service,[34][35] which would have forced him to participate in the apartheid regime,[1] as well as to ease his path to immigration to the United States.[36] While waiting for his application to be processed, he attended the University of Pretoria for five months.[37]
-
-Musk arrived in Canada in June 1989, connected with a second cousin in Saskatchewan,[38][39] and worked odd jobs, including at a farm and a lumber mill.[40] In 1990, he entered Queen's University in Kingston, Ontario.[41][42] Two years later, he transferred to the University of Pennsylvania, where he studied until 1995.[43] Although Musk has said that he earned his degrees in 1995, the University of Pennsylvania did not award them until 1997 – a Bachelor of Arts in physics and a Bachelor of Science in economics from the university's Wharton School.[44][45][46][47][48] He reportedly hosted large, ticketed house parties to help pay for tuition, and wrote a business plan for an electronic book-scanning service similar to Google Books.[49]
-
-In 1994, Musk held two internships in Silicon Valley: one at energy storage startup Pinnacle Research Institute, which investigated electrolytic supercapacitors for energy storage, and another at Palo Alto–based startup Rocket Science Games.[50][51] In 1995, he was accepted to a graduate program in materials science at Stanford University, but did not enroll.[46][44][52] Musk decided to join the Internet boom of the 1990s, applying for a job at Netscape, to which he reportedly never received a response.[53][34] The Washington Post reported that Musk lacked legal authorization to remain and work in the United States after failing to enroll at Stanford.[52] In response, Musk said he was allowed to work at that time and that his student visa transitioned to an H1-B. According to numerous former business associates and shareholders, Musk said he was on a student visa at the time.[54]
-
-Business career
-Main article: Business career of Elon Musk
-Zip2
-Main article: Zip2
-External videos
-video icon Musk speaks of his early business experience during a 2014 commencement speech at University of Southern California on YouTube
-In 1995, Musk, his brother Kimbal, and Greg Kouri founded the web software company Zip2 with funding from a group of angel investors.[55] They housed the venture at a small rented office in Palo Alto.[56] Replying to Rolling Stone, Musk denounced the notion that they started their company with funds borrowed from Elon's father Errol Musk,[24] but in a tweet, he recognized that his father contributed 10% of a later funding round.[57] The company developed and marketed an Internet city guide for the newspaper publishing industry, with maps, directions, and yellow pages.[58]
-
-According to Musk, "The website was up during the day and I was coding it at night, seven days a week, all the time."[56] To impress investors, Musk built a large plastic structure around a standard computer to create the impression that Zip2 was powered by a small supercomputer.[59] The Musk brothers obtained contracts with The New York Times and the Chicago Tribune,[60] and persuaded the board of directors to abandon plans for a merger with CitySearch.[61] Musk's attempts to become CEO were thwarted by the board.[62] Compaq acquired Zip2 for $307 million in cash in February 1999 (equivalent to $590,000,000 in 2025),[63][64] and Musk received $22 million (equivalent to $43,000,000 in 2025) for his 7-percent share.[65]
-
-X.com and PayPal
-Main articles: X.com (bank), PayPal, and PayPal Mafia
-In 1999, Musk co-founded X.com, an online financial services and e-mail payment company.[66] The startup was one of the first federally insured online banks, and, in its initial months of operation, over 200,000 customers joined the service.[67] The company's investors regarded Musk as inexperienced and replaced him with Intuit CEO Bill Harris by the end of the year.[68] The following year, X.com merged with online bank Confinity to avoid competition.[56][68][69] Founded by Max Levchin and Peter Thiel,[70] Confinity had its own money-transfer service, PayPal, which was more popular than X.com's service.[71]
-
-Within the merged company, Musk returned as CEO. Musk's preference for Microsoft software over Unix created a rift in the company and caused Thiel to resign.[72] Due to resulting technological issues and lack of a cohesive business model, the board ousted Musk and replaced him with Thiel in 2000.[73][b] Under Thiel, the company focused on the PayPal service and was renamed PayPal in 2001.[75][76] In 2002, PayPal was acquired by eBay for $1.5 billion (equivalent to $2,700,000,000 in 2025) in stock, of which Musk—the largest shareholder with 11.72% of shares—received $175.8 million (equivalent to $320,000,000 in 2025).[77][78] In 2017, Musk purchased the domain X.com from PayPal for an undisclosed amount, stating that it had sentimental value.[79][80]
-
-SpaceX
-Main article: SpaceX
-Musk tours SpaceX with President Barack Obama in 2010.
-
-Musk explains Starship capabilities to leaders of North American Aerospace Defense Command, U.S. Northern Command, and Air Force Space Command in 2019.
-In 2001, Musk became involved with the nonprofit Mars Society and discussed funding plans to place a growth-chamber for plants on Mars.[81] Seeking a way to launch the greenhouse payloads into space, Musk made two unsuccessful trips to Moscow to purchase intercontinental ballistic missiles (ICBMs) from Russian companies NPO Lavochkin and Kosmotras. Musk instead decided to start a company to build affordable rockets.[82] With $100 million of his early fortune,[83] (equivalent to $180,000,000 in 2025) Musk founded SpaceX in May 2002 and became the company's CEO and Chief Engineer.[84][85]
-
-SpaceX attempted its first launch of the Falcon 1 rocket in 2006.[86] Although the rocket failed to reach Earth orbit, it was awarded a Commercial Orbital Transportation Services program contract from NASA, then led by Mike Griffin.[87][88] After two more failed attempts that nearly caused Musk to go bankrupt,[86] SpaceX succeeded in launching the Falcon 1 into orbit in 2008.[89] Later that year, SpaceX received a $1.6 billion NASA contract (equivalent to $2,400,000,000 in 2025) for Falcon 9-launched Dragon spacecraft flights to the International Space Station (ISS), replacing the Space Shuttle after its 2011 retirement.[90] In 2012, the Dragon vehicle docked with the ISS, a first for a commercial spacecraft.[91]
-
-Working towards its goal of reusable rockets, in 2015 SpaceX successfully landed the first stage of a Falcon 9 on a land platform.[92] Later landings were achieved on autonomous spaceport drone ships, an ocean-based recovery platform.[93] In 2018, SpaceX launched the Falcon Heavy; the inaugural mission carried Musk's personal Tesla Roadster as a dummy payload.[94][95] Since 2019,[96] SpaceX has been developing Starship, a reusable, super heavy-lift launch vehicle intended to replace the Falcon 9 and Falcon Heavy.[97] In 2020, SpaceX launched its first crewed flight, the Demo-2, becoming the first private company to place astronauts into orbit and dock a crewed spacecraft with the ISS.[98] In 2024, NASA awarded SpaceX an $843 million (equivalent to $865,000,000 in 2025) contract to build a spacecraft that NASA will use to deorbit the ISS at the end of its lifespan.[99]
-
-Starlink
-Main article: Starlink
-See also: Starlink in the Russo-Ukrainian War
-
-50 Starlink satellites shortly before deployment to low Earth orbit, 2019
-In 2015, SpaceX began development of the Starlink constellation of low Earth orbit satellites to provide satellite Internet access.[100] After the launch of prototype satellites in 2018, the first large constellation was deployed in May 2019.[101] As of May 2025, over 7,600 Starlink satellites are operational,[102] comprising 65% of all operational Earth satellites.[103] The total cost of the decade-long project to design, build, and deploy the constellation was estimated by SpaceX in 2020 to be $10 billion (equivalent to $12,000,000,000 in 2025).[104][c]
-
-During the Russian invasion of Ukraine, Musk provided free Starlink service to Ukraine, permitting Internet access and communication at a yearly cost to SpaceX of $400 million (equivalent to $440,000,000 in 2025).[107][108][109][110][111] However, Musk refused to block Russian state media on Starlink.[112][113] In 2023, Musk denied Ukraine's request to activate Starlink over Crimea to aid an attack against the Russian navy, citing fears of a nuclear response.[114][115][116]
-
-usk uses a private jet owned by Falcon Landing LLC, a SpaceX-linked company, and acquired a second jet in August 2020.[216][217] His heavy use of the jets and the consequent fossil fuel usage have received criticism.[216][218] Musk's flight usage is tracked on social media through ElonJet.[219][220][221] In December 2022, Musk banned the ElonJet account on Twitter, and made temporary bans on the accounts of journalists that posted stories regarding the incident, including Donie O'Sullivan, Keith Olbermann, and journalists from The New York Times, The Washington Post, CNN, and The Intercept.[222]
+    Returns:
+        The search result 
+    
     """
-    summary_template = """
-    Given the information {information} about person i want you to create
-    1. Short Summary 
-    2. Two Intresting fact about them
-     """
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"],
-        template=summary_template
-    )
+    print(f'Searching for {query}')
+    return f"Tokyo weather is sunny"
 
-    llm = ChatOpenAI(temperature=0.5,model="gpt-5-nano")
-    # llm = ChatOllama(temperature=0.5,model="gpt-oss:120b-cloud")
+llm=  ChatOllama()
 
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information":information})
-    print(response.content)
+tools = [search_query]
+
+agent = create_agent(model= llm,tools = tools)
+
 if __name__ == "__main__":
     main()
