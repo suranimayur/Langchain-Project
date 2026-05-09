d2bf13b5672dcfa3975112637dab39ff720450b4
suranimayur <surani.mayur@gmail.com>
2026-05-09 21:37:07 +0530

    Intermediate commit

--- ingestion.py
+++ ingestion.py
@@ -1,51 +1,33 @@
+import asyncio
+import logging
 import os
-from dotenv import load_dotenv
+import sys
+import ssl
+from typing import List, Dict, Any
 
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+import certifi
+from dotenv import load_dotenv
+from langchain_text_splitters  import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAI, OpenAIEmbeddings # Corrected import
 from langchain_pinecone import PineconeVectorStore
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_header, log_info, log_success, log_warning)
 
 load_dotenv()
 
-def main():
-    try:
-        print("🚀 Starting ingestion...")
+# Configure SSL context to use certifi certificates
 
-        loader = TextLoader(
-            r"D:\Udemy\Build AI Agents with Langchain\medium_blog.txt",
-            encoding="utf-8"
-        )
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
-        docs = loader.load()
 
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=100
-        )
+async def main():
+    print("Main async function to orchestrate the ingestion process.")
 
-        texts = splitter.split_documents(docs)
-
-        print(f"✅ Created {len(texts)} chunks")
-
-        # FIXED MODEL
-        embeddings = OpenAIEmbeddings(
-            api_key=os.getenv("OPENAI_API_KEY"),
-            model="text-embedding-3-large"
-        )
-
-        PineconeVectorStore.from_documents(
-            documents=texts,
-            embedding=embeddings,
-            index_name=os.getenv("INDEX_NAME")
-        )
-
-        print("🎉 Upload Success!")
-
-    except Exception as e:
-        print("❌ Error:", e)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
